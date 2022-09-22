c8fddfb53af45493db33b0a781a1974071726885
syp00 <syp0530@gmail.com>
2022-09-22 11:33:14 +0900

    cartdto fin

--- smartPhone/src/main/java/com/multi/dto/CartDTO.java
+++ smartPhone/src/main/java/com/multi/dto/CartDTO.java
@@ -1,5 +1,23 @@
 package com.multi.dto;
 
-public class CartDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class CartDTO {
+	private int cartid;
+	private String cust_id;
+	private int item_id;
+	private int cnt;
+	private String cust_name;
+	private String item_pname;
+	private int item_price;
 }
