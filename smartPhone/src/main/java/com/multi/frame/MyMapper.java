3cece80a075296c866fff6442f50e7ebf69cd889
jaeyooon <jycho0208@gmail.com>
2022-09-22 16:30:08 +0900

    modify MyMapper, MyService

--- smartPhone/src/main/java/com/multi/frame/MyMapper.java
+++ smartPhone/src/main/java/com/multi/frame/MyMapper.java
@@ -3,13 +3,9 @@ package com.multi.frame;
 import java.util.List;
 
 public interface MyMapper<K, V> {
-	void insert(V v) throws Exception;
-	
-	void delete(K k) throws Exception;
-	
-	void update(V v) throws Exception;
-	
-	V select(K k) throws Exception;
-	
-	List<V> selectall() throws Exception;
+	public void insert(V v) throws Exception;
+	public void delete(K k) throws Exception;
+	public void update(V v) throws Exception;
+	public V select(K k) throws Exception; 
+	public List<V> selectall() throws Exception;
 }
--- smartPhone/src/main/java/com/multi/frame/MyService.java
+++ smartPhone/src/main/java/com/multi/frame/MyService.java
@@ -3,13 +3,9 @@ package com.multi.frame;
 import java.util.List;
 
 public interface MyService <K, V> {
-    void register(V v) throws Exception;
-	
-	void remove(K k) throws Exception;
-	
-	void modify(V v) throws Exception;
-	
-	V get(K k) throws Exception;
-	
-	List<V> selectall() throws Exception;
+	public void register(V v) throws Exception;
+	public void remove(K k) throws Exception;
+	public void modify(V v) throws Exception;
+	public V get(K k) throws Exception;
+	public List<V> get() throws Exception;
 }
