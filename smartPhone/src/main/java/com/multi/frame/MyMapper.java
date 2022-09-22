8c58a17c23c19f8ce3d892efa0807982ef003c2e
wanttobodybuilderifbbpro <wogud114@gmail.com>
2022-09-22 11:42:55 +0900

    Two things(mapper, service) have been completed.

--- smartPhone/src/main/java/com/multi/frame/MyMapper.java
+++ smartPhone/src/main/java/com/multi/frame/MyMapper.java
@@ -1,5 +1,15 @@
 package com.multi.frame;
 
-public interface MyMapper {
+import java.util.List;
 
+public interface MyMapper<K, V> {
+	void insert(V v) throws Exception;
+	
+	void delete(K k) throws Exception;
+	
+	void update(V v) throws Exception;
+	
+	V select(K k) throws Exception;
+	
+	List<V> selectall() throws Exception;
 }
--- smartPhone/src/main/java/com/multi/frame/MyService.java
+++ smartPhone/src/main/java/com/multi/frame/MyService.java
@@ -1,5 +1,15 @@
 package com.multi.frame;
 
-public interface MyService {
+import java.util.List;
 
+public interface MyService <K, V> {
+    void register(V v) throws Exception;
+	
+	void remove(K k) throws Exception;
+	
+	void modify(V v) throws Exception;
+	
+	V get(K k) throws Exception;
+	
+	List<V> selectall() throws Exception;
 }
