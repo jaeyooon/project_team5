b56cea88774489fa92a9115a52e36a813f54e221
syp00 <syp0530@gmail.com>
2022-09-22 13:37:46 +0900

    cartmapper

--- smartPhone/src/main/java/com/multi/mapper/CartMapper.java
+++ smartPhone/src/main/java/com/multi/mapper/CartMapper.java
@@ -1,5 +1,10 @@
 package com.multi.mapper;
 
-public interface CartMapper {
+import java.util.List;
 
+import com.multi.dto.CartDTO;
+import com.multi.frame.MyMapper;
+
+public interface CartMapper extends MyMapper<Integer, CartDTO> {
+	public List<CartDTO> cartall(String cust_id) throws Exception;
 }
